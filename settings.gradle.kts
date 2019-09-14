3a9e2662acb40d99d405bef94e25baccc86ac755
Erik Christensen <erikchristensen@comcast.net>
2019-09-14 12:55:01 -0400

    Use plugin management for Android Gradle plugin instead of classpath

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,22 @@
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "com.android.library" -> useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+        }
+    }
+
+    repositories {
+        google()
+        gradlePluginPortal()
+    }
+}
+
+enableFeaturePreview("GRADLE_METADATA")
+
 include(
     ":core",
     ":tools:code-generator",
     ":extensions:threetenabp"
 )
-
-enableFeaturePreview("GRADLE_METADATA")
