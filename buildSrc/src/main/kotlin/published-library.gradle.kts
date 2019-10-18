fd5fbda829fe08a46b0b902acac6a9adeaeb279a
Erik Christensen <erikchristensen@comcast.net>
2019-10-18 18:45:34 -0400

    Added full Maven publishing support

---
+++ buildSrc/src/main/kotlin/published-library.gradle.kts
@@ -0,0 +1,97 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+
+plugins {
+    id("org.jetbrains.dokka")
+    `maven-publish`
+    signing
+}
+
+val Project.isReleaseBuild get() = !version.toString().endsWith("SNAPSHOT")
+
+val dokka by tasks.existing(DokkaTask::class) {
+    outputDirectory = "$buildDir/dokka"
+    outputFormat = "html"
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    dependsOn(dokka)
+    archiveClassifier.set("javadoc")
+    from(dokka.get().outputDirectory)
+}
+
+@Suppress("UnstableApiUsage")
+signing {
+    setRequired { isReleaseBuild && gradle.taskGraph.hasTask("uploadArchives") }
+
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications)
+}
+
+afterEvaluate {
+    publishing {
+        repositories {
+            maven {
+                val snapshotRepositoryUrl: String by project
+                val releaseRepositoryUrl: String by project
+                val repositoryUrl = if (isReleaseBuild) releaseRepositoryUrl else snapshotRepositoryUrl
+
+                url = uri(repositoryUrl)
+
+                val repositoryUsername: String? by project
+                val repositoryPassword: String? by project
+
+                credentials {
+                    username = repositoryUsername.orEmpty()
+                    password = repositoryPassword.orEmpty()
+                }
+            }
+        }
+
+        publications.withType<MavenPublication>().configureEach {
+            artifact(javadocJar.get())
+
+            val pomName: String by project
+            val pomDescription: String by project
+            val pomScmUrl: String? by project
+            val pomUrl: String? by project
+            val pomScmConnection: String? by project
+            val pomLicenseName: String? by project
+            val pomLicenseUrl: String? by project
+            val pomLicenseDist: String? by project
+            val pomDeveloperId: String? by project
+            val pomDeveloperName: String? by project
+            val pomArtifactId: String? by project
+
+            if (pomArtifactId != null) {
+                artifactId = pomArtifactId
+            }
+
+            @Suppress("UnstableApiUsage")
+            pom.apply {
+                name.set(pomName)
+                description.set(pomDescription)
+                url.set(pomUrl)
+                licenses {
+                    license {
+                        name.set(pomLicenseName)
+                        url.set(pomLicenseUrl)
+                        distribution.set(pomLicenseDist)
+                    }
+                }
+                developers {
+                    developer {
+                        id.set(pomDeveloperId)
+                        name.set(pomDeveloperName)
+
+                    }
+                }
+                scm {
+                    connection.set(pomScmConnection)
+                    url.set(pomScmUrl)
+                }
+            }
+        }
+    }
+}
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,33 +1,13 @@
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-import org.jetbrains.kotlin.konan.target.HostManager
-
 plugins {
-    kotlin("multiplatform")
-    `maven-publish`
-    signing
+    `multiplatform-library`
 }
 
 kotlin {
     jvm {
         withJava()
     }
-//    js {
-//        useCommonJs()
-//        browser()
-//        nodejs()
-//    }
-    iosArm64()
-    iosX64()
-//    iosX64("ios")
 
     sourceSets {
-        all {
-            languageSettings.apply {
-                enableLanguageFeature("InlineClasses")
-                progressiveMode = true
-            }
-        }
-
         val commonMain by getting {
             kotlin.srcDirs("src/commonMain/generated", "src/commonMain/kotlin")
 
@@ -36,17 +16,20 @@ kotlin {
                 implementation("co.touchlab:stately:0.9.3")
             }
         }
+
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
             }
         }
+
         val jvmMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-jdk8"))
             }
         }
+
         val jvmTest by getting {
             dependencies {
                 implementation(kotlin("test"))
@@ -54,6 +37,7 @@ kotlin {
                 implementation("com.google.truth:truth:1.0")
             }
         }
+
 //        val jsMain by getting {
 //            dependencies {
 //                implementation(kotlin("stdlib-js"))
@@ -64,53 +48,11 @@ kotlin {
 //                implementation(kotlin("test-js"))
 //            }
 //        }
-//        val iosMain by getting {
-      val iosMain by creating {
-            dependsOn(commonMain)
 
+        val iosMain by getting {
             dependencies {
                 implementation("co.touchlab:stately-collections:0.9.3")
             }
         }
-//        val iosTest by getting
-        val iosTest by creating {
-            dependsOn(commonTest)
-        }
-    }
-
-    configure(listOf(iosArm64(), iosX64())) {
-//    configure(listOf(iosX64("ios"))) {
-        compilations.getByName("main") {
-            source(sourceSets.getByName("iosMain"))
-            extraOpts.add("-Xobjc-generics")
-        }
-        compilations["test"].source(sourceSets.getByName("iosTest"))
-
-        binaries.framework {
-            baseName = "IslandTimeCore"
-        }
-    }
-}
-
-if (HostManager.hostIsMac) {
-    tasks.register("iosTest") {
-        val device = project.findProperty("iosDevice")?.toString() ?: "iPhone Xʀ"
-        dependsOn(tasks.named("linkDebugTestIosX64"))
-//        dependsOn(tasks.named("linkDebugTestIos"))
-        group = JavaBasePlugin.VERIFICATION_GROUP
-        description = "Run tests for target 'ios' on an iOS simulator"
-
-        doLast {
-            val binary = (kotlin.targets["iosX64"] as KotlinNativeTarget).binaries.getTest("DEBUG").outputFile
-//            val binary = (kotlin.targets["ios"] as KotlinNativeTarget).binaries.getTest("DEBUG").outputFile
-
-            exec {
-                commandLine("xcrun", "simctl", "spawn", "--standalone", device, binary.absolutePath)
-            }
-        }
-    }
-
-    tasks.named("check") {
-        dependsOn(tasks.named("iosTest"))
     }
 }
