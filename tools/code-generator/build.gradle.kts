5938130015f76181686173aa6049fedf55ef3127
Erik Christensen <erikchristensen@comcast.net>
2019-10-28 17:22:37 -0400

    Bump KotlinPoet to 1.4.1

--- tools/code-generator/build.gradle.kts
+++ tools/code-generator/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("com.squareup:kotlinpoet:1.4.0")
+    implementation("com.squareup:kotlinpoet:1.4.1")
 }
 
 application {
