1041634185ebd948141fd3d5155e46046d9d9620
Erik Christensen <40830816+erikc5000@users.noreply.github.com>
2019-12-11 14:15:10 -0500

    Fix test/CI issues (#18)
    * Try workaround for new emulator
    
    https://issuetracker.google.com/issues/145622251#comment33
    
    * Add more test logging
    
    * Show stack traces in test logging
    
    * More test logging changes
    
    * Fix Gradle
    
    * Try this again
    
    * Again
    
    * Test environment fixes

--- extensions/parcelize/src/androidTest/kotlin/io/islandtime/extensions/parcelize/ranges/ZonedDateTimeIntervalTest.kt
+++ extensions/parcelize/src/androidTest/kotlin/io/islandtime/extensions/parcelize/ranges/ZonedDateTimeIntervalTest.kt
@@ -1,14 +1,17 @@
 package io.islandtime.extensions.parcelize.ranges
 
 import android.os.Parcelable
+import androidx.test.platform.app.InstrumentationRegistry
+import io.islandtime.IslandTime
 import io.islandtime.ZonedDateTime
 import io.islandtime.ranges.ZonedDateTimeInterval
 import io.islandtime.TimeZone
 import io.islandtime.extensions.parcelize.test.testParcelable
+import io.islandtime.extensions.threetenabp.AndroidThreeTenProvider
 import io.islandtime.ranges.until
 import kotlinx.android.parcel.Parcelize
 import kotlinx.android.parcel.TypeParceler
-import org.junit.Test
+import org.junit.*
 
 class ZonedDateTimeIntervalRangeTest {
     @Parcelize
@@ -27,5 +30,22 @@ class ZonedDateTimeIntervalRangeTest {
     @Test
     fun zonedDateTimeIntervalParceler() {
         testZonedDateTimeIntervals.forEach { testParcelable(TestData(it)) }
+    }
+
+    companion object {
+        private val context = InstrumentationRegistry.getInstrumentation().targetContext.applicationContext
+
+        @JvmStatic
+        @BeforeClass
+        fun setUp() {
+            IslandTime.initializeWith(AndroidThreeTenProvider(context))
+        }
+
+
+        @JvmStatic
+        @AfterClass
+        fun tearDown() {
+            IslandTime.reset()
+        }
     }
 }
