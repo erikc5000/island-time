ffd968b4bc54f34f16c81ce0e511fa0607e9db8f
Erik Christensen <erikchristensen@comcast.net>
2019-12-27 17:36:59 -0500

    Add support for locale-based NumberStyle on Darwin

--- core/src/darwinMain/kotlin/io/islandtime/format/NumberStyle.kt
+++ core/src/darwinMain/kotlin/io/islandtime/format/NumberStyle.kt
@@ -1,6 +1,18 @@
 package io.islandtime.format
 
 import io.islandtime.locale.Locale
+import platform.Foundation.NSNumber
+import platform.Foundation.NSNumberFormatter
+import platform.Foundation.decimalSeparator
 
 actual val Locale.numberStyle: NumberStyle
-    get() = TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    get() {
+        val formatter = NSNumberFormatter().also { it.locale = this }
+
+        return NumberStyle(
+            zeroDigit = formatter.stringFromNumber(NSNumber(int = 0))?.firstOrNull() ?: '0',
+            plusSign = listOf(formatter.plusSign.firstOrNull() ?: '+'),
+            minusSign = listOf(formatter.minusSign.firstOrNull() ?: '-'),
+            decimalSeparator = listOf(decimalSeparator.firstOrNull() ?: '.')
+        )
+    }
