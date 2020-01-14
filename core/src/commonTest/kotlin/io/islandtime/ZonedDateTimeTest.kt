eb7dd313213238530d35cb2aa4598d59cccdd632
Erik Christensen <40830816+erikc5000@users.noreply.github.com>
2020-01-14 17:52:37 -0500

    By default, respect the system setting for first day of week (#28)

--- core/src/commonTest/kotlin/io/islandtime/ZonedDateTimeTest.kt
+++ core/src/commonTest/kotlin/io/islandtime/ZonedDateTimeTest.kt
@@ -650,4 +650,30 @@ class ZonedDateTimeTest : AbstractIslandTimeTest() {
             "2017-02-28T14:00:00.123456789-07:00".toOffsetDateTime().asZonedDateTime()
         )
     }
+
+    @Test
+    fun `Date_startOfDayAt() creates a ZonedDateTime at the start of the day in a particular time zone`() {
+        assertEquals(
+            ZonedDateTime(
+                DateTime(2019, 5, 20, 0, 0),
+                nyZone
+            ),
+            Date(2019, 5, 20).startOfDayAt(nyZone)
+        )
+
+        // TODO: Add tests where transitions occur during midnight
+    }
+
+    @Test
+    fun `Date_endOfDayAt() creates a ZonedDateTime at the end of the day in a particular time zone`() {
+        assertEquals(
+            ZonedDateTime(
+                DateTime(2019, 5, 20, 23, 59, 59, 999_999_999),
+                nyZone
+            ),
+            Date(2019, 5, 20).endOfDayAt(nyZone)
+        )
+
+        // TODO: Add tests where transitions occur during midnight
+    }
 }
--- core/src/darwinMain/kotlin/io/islandtime/DarwinDayOfWeek.kt
+++ core/src/darwinMain/kotlin/io/islandtime/DarwinDayOfWeek.kt
@@ -4,18 +4,15 @@ import io.islandtime.locale.Locale
 import io.islandtime.measures.days
 import platform.Foundation.NSCalendar
 import platform.Foundation.NSCalendarIdentifierGregorian
-import platform.Foundation.NSLocale
-import platform.Foundation.currentLocale
+
+internal actual val Locale.firstDayOfWeek: DayOfWeek
+    get() = NSCalendar(NSCalendarIdentifierGregorian).also { it.locale = this }.firstDayOfWeek
+
+internal actual fun systemDefaultFirstDayOfWeek(): DayOfWeek = NSCalendar.currentCalendar.firstDayOfWeek
 
 @UseExperimental(ExperimentalUnsignedTypes::class)
-internal actual val Locale.firstDayOfWeek
-    get(): DayOfWeek {
-        val calendar = NSCalendar.calendarWithIdentifier(NSCalendarIdentifierGregorian)?.also {
-            it.locale = this
-        } ?: throw IllegalStateException("Unable to create calendar")
-
-        val sundayIndexedWeekNumber = calendar.firstWeekday.toInt()
+private val NSCalendar.firstDayOfWeek: DayOfWeek
+    get() {
+        val sundayIndexedWeekNumber = firstWeekday.toInt()
         return DayOfWeek.SUNDAY + (sundayIndexedWeekNumber - 1).days
     }
-
-internal actual val Locale.lastDayOfWeek: DayOfWeek get() = firstDayOfWeek + 6.days
