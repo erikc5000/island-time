44a5f54d27c408899c2cb54a5e908ba8e19a444e
Erik Christensen <40830816+erikc5000@users.noreply.github.com>
2020-02-04 09:36:09 -0500

    Bump AGP to 3.6.0-rc02, Dokka to 0.10.1, Gradle to 6.1.1 (#33)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,8 +9,8 @@ plugins {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:0.10.0")
-    implementation("com.android.tools.build:gradle:3.6.0-rc01")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:0.10.1")
+    implementation("com.android.tools.build:gradle:3.6.0-rc02")
 }
 
 kotlinDslPluginOptions {
