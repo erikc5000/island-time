e7c9378b861b12c4a070035dd85ce956bbd90480
Erik Christensen <40830816+erikc5000@users.noreply.github.com>
2020-03-30 15:49:52 -0400

    Clean up build scripts (#56)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,7 @@
 object Versions {
     const val kotlin = "1.3.71"
     const val serialization = "0.20.0"
+    const val atomicfu = "0.14.2"
     const val androidxTest = "1.2.0"
     const val googleTruth = "1.0.1"
     const val threetenabp = "1.2.3"
@@ -14,6 +15,8 @@ object Libs {
         const val jvm = "org.jetbrains.kotlinx:kotlinx-serialization-runtime:${Versions.serialization}"
         const val native = "org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:${Versions.serialization}"
     }
+
+    const val atomicfu = "org.jetbrains.kotlinx:atomicfu-gradle-plugin:${Versions.atomicfu}"
 
     object AndroidxTest {
         const val runner = "androidx.test:runner:${Versions.androidxTest}"
--- buildSrc/src/main/kotlin/multiplatform-library.gradle.kts
+++ buildSrc/src/main/kotlin/multiplatform-library.gradle.kts
@@ -1,6 +1,5 @@
 import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
 
 plugins {
     kotlin("multiplatform")
