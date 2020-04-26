1c65a8a331fea0503999e7d40c2a78e602619b8a
Erik Christensen <erikchristensen@comcast.net>
2020-04-26 15:29:22 -0400

    Add convenience functions for Temporal property access

--- core/src/commonMain/kotlin/io/islandtime/base/Temporal.kt
+++ core/src/commonMain/kotlin/io/islandtime/base/Temporal.kt
@@ -11,6 +11,11 @@ import io.islandtime.DateTimeException
 interface TemporalProperty<T>
 
 /**
+ * A property associated with a `Boolean` value.
+ */
+interface BooleanProperty : TemporalProperty<Boolean>
+
+/**
  * A property associated with a `Long` value.
  */
 interface NumberProperty : TemporalProperty<Long> {
@@ -19,11 +24,6 @@ interface NumberProperty : TemporalProperty<Long> {
      */
     val valueRange: LongRange get() = Long.MIN_VALUE..Long.MAX_VALUE
 }
-
-/**
- * A property associated with a `Boolean` value.
- */
-interface BooleanProperty : TemporalProperty<Boolean>
 
 /**
  * A property associated with an object value.
@@ -44,19 +44,97 @@ class TemporalPropertyException(
  * A framework-level interface providing access to the properties of a date, time, or measurement of time.
  */
 interface Temporal {
+    /**
+     * Check if [property] is available.
+     */
     fun has(property: TemporalProperty<*>): Boolean = false
 
-    fun <T> get(property: ObjectProperty<T>): T {
-        throwUnsupportedTemporalPropertyException(property)
-    }
-
+    /**
+     * Get the value of [property].
+     * @throws TemporalPropertyException if the property isn't available
+     */
     fun get(property: BooleanProperty): Boolean {
         throwUnsupportedTemporalPropertyException(property)
     }
 
+    /**
+     * Get the value of [property].
+     * @throws TemporalPropertyException if the property isn't available
+     */
     fun get(property: NumberProperty): Long {
         throwUnsupportedTemporalPropertyException(property)
     }
+
+    /**
+     * Get the value of [property].
+     * @throws TemporalPropertyException if the property isn't available
+     */
+    fun <T> get(property: ObjectProperty<T>): T {
+        throwUnsupportedTemporalPropertyException(property)
+    }
+}
+
+/**
+ * Get the value of [property] or the result of [defaultValue] if it isn't available.
+ */
+inline fun Temporal.getOrElse(
+    property: BooleanProperty,
+    defaultValue: (property: BooleanProperty) -> Boolean
+): Boolean {
+    return if (has(property)) {
+        get(property)
+    } else {
+        defaultValue(property)
+    }
+}
+
+/**
+ * Get the value of [property] or the result of [defaultValue] if it isn't available.
+ */
+inline fun Temporal.getOrElse(
+    property: NumberProperty,
+    defaultValue: (property: NumberProperty) -> Long
+): Long {
+    return if (has(property)) {
+        get(property)
+    } else {
+        defaultValue(property)
+    }
+}
+
+/**
+ * Get the value of [property] or the result of [defaultValue] if it isn't available.
+ */
+inline fun <T> Temporal.getOrElse(
+    property: ObjectProperty<T>,
+    defaultValue: (property: ObjectProperty<T>) -> T
+): T {
+    return if (has(property)) {
+        get(property)
+    } else {
+        defaultValue(property)
+    }
+}
+
+/**
+ * Get the value of [property] or `null`` if it isn't available.
+ */
+fun Temporal.getOrNull(property: BooleanProperty): Boolean? {
+    return if (has(property)) get(property) else null
+}
+
+/**
+ * Get the value of [property] or `null`` if it isn't available.
+ */
+fun Temporal.getOrNull(property: NumberProperty): Long? {
+    return if (has(property)) get(property) else null
+}
+
+/**
+ * Get the value of [property] or `null`` if it isn't available.
+ */
+fun <T> Temporal.getOrNull(property: ObjectProperty<T>): T? {
+    return if (has(property)) get(property) else null
 }
 
 internal fun throwUnsupportedTemporalPropertyException(property: TemporalProperty<*>): Nothing {
