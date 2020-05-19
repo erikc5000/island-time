6037b9bc72a59217a65374a3c4124729ea4b1998
Erik Christensen <erikchristensen@comcast.net>
2020-05-18 22:14:35 -0400

    Bump AGP to 4.0.0-rc01

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:0.10.1")
-    implementation("com.android.tools.build:gradle:4.0.0-beta04")
+    implementation("com.android.tools.build:gradle:4.0.0-rc01")
 }
 
 kotlinDslPluginOptions {
