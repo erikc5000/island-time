0264432dfe6d5bfe292e8ff9ee44367e84d88a47
Erik Christensen <erikchristensen@comcast.net>
2020-07-10 21:26:02 -0400

    Update to Kotlin 1.4-M3

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,11 +1,11 @@
 object Versions {
-    const val kotlin = "1.4-M2"
-    const val serialization = "0.20.0-1.4-M2"
-    const val atomicfu = "0.14.3-1.4-M2"
+    const val kotlin = "1.4-M3"
+    const val serialization = "0.20.0-1.4-M3"
+    const val atomicfu = "0.14.3-1.4-M3"
     const val androidxTest = "1.2.0"
     const val googleTruth = "1.0.1"
     const val threetenabp = "1.2.4"
-    const val stately = "1.0.4-a1-1.4-M2"
+    const val stately = "1.0.4-a1-1.4-M3"
     const val kotlinpoet = "1.6.0"
     const val androidDesugarJdkLibs = "1.0.9"
 }
