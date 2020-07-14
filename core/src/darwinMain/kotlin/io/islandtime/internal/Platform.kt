70e4e46e5c3c9f87a378a0398b46ee746fa792ed
Erik Christensen <erikchristensen@comcast.net>
2020-07-14 17:19:08 -0400

    Fix missing Darwin import

--- core/src/darwinMain/kotlin/io/islandtime/internal/Platform.kt
+++ core/src/darwinMain/kotlin/io/islandtime/internal/Platform.kt
@@ -3,5 +3,6 @@ package io.islandtime.internal
 import io.islandtime.TimeZone
 import io.islandtime.darwin.toIslandTimeZone
 import platform.Foundation.NSTimeZone
+import platform.Foundation.localTimeZone
 
 internal actual fun systemDefaultTimeZone(): TimeZone = NSTimeZone.localTimeZone.toIslandTimeZone()
