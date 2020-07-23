d8385effec160025a40b829a4e873cb79b528eb0
Erik Christensen <40830816+erikc5000@users.noreply.github.com>
2020-07-22 20:48:43 -0400

    Add week number and year properties (#104)
    * Validate minimumDAysInFirstWeek in WeekSettings constructor
    
    * Add week number and year properties

--- tools/code-generator/src/main/kotlin/io/islandtime/codegen/Main.kt
+++ tools/code-generator/src/main/kotlin/io/islandtime/codegen/Main.kt
@@ -1,13 +1,29 @@
 package io.islandtime.codegen
 
+import com.squareup.kotlinpoet.ClassName
+import io.islandtime.codegen.generators.ConstantsGenerator
+import io.islandtime.codegen.generators.DatePropertiesGenerator
+import io.islandtime.codegen.generators.TemporalUnitGenerator
 import java.io.File
 
-const val OUTPUT_PATH = "../../core/src/commonMain/generated"
+private const val OUTPUT_PATH = "../../core/src/commonMain/generated"
 
-const val MEASURES_PACKAGE_NAME = "io.islandtime.measures"
-const val INTERNAL_PACKAGE_NAME = "io.islandtime.internal"
+private const val BASE_PACKAGE_NAME = "io.islandtime"
+private const val MEASURES_PACKAGE_NAME = "io.islandtime.measures"
+private const val CALENDAR_PACKAGE_NAME = "io.islandtime.calendar"
+private const val INTERNAL_PACKAGE_NAME = "io.islandtime.internal"
+
+fun base(name: String) = ClassName(BASE_PACKAGE_NAME, name)
+fun internal(name: String) = ClassName(INTERNAL_PACKAGE_NAME, name)
+fun measures(name: String) = ClassName(MEASURES_PACKAGE_NAME, name)
+fun calendar(name: String) = ClassName(CALENDAR_PACKAGE_NAME, name)
+
+private val generators = arrayOf(
+    TemporalUnitGenerator,
+    ConstantsGenerator,
+    DatePropertiesGenerator
+)
 
 fun main() {
-    (generateTemporalUnitFileSpecs() + generateConstantsFileSpec())
-        .forEach { it.writeTo(File(OUTPUT_PATH)) }
+    generators.flatMap { it.generate() }.forEach { it.writeTo(File(OUTPUT_PATH)) }
 }
