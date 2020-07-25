d54762faf7e68a51da6d5d40e41aa147fdf92d19
Erik Christensen <erikchristensen@comcast.net>
2020-07-24 21:30:02 -0400

    Expand code generation to include conversions between date types

--- tools/code-generator/src/main/kotlin/io/islandtime/codegen/descriptions/DateTimeDescription.kt
+++ tools/code-generator/src/main/kotlin/io/islandtime/codegen/descriptions/DateTimeDescription.kt
@@ -4,35 +4,63 @@ import com.squareup.kotlinpoet.TypeName
 import io.islandtime.codegen.base
 
 enum class DateTimeDescription {
+    Year {
+        override val typeName: TypeName get() = base("Year")
+        override val smallestUnit: TemporalUnitDescription get() = TemporalUnitDescription.YEARS
+        override val simpleName: String get() = "year"
+        override val isDateBased: Boolean = true
+    },
+    YearMonth {
+        override val typeName: TypeName get() = base("YearMonth")
+        override val smallestUnit: TemporalUnitDescription get() = TemporalUnitDescription.MONTHS
+        override val simpleName: String get() = "year-month"
+        override val isDateBased: Boolean = true
+    },
     Date {
         override val typeName: TypeName get() = base("Date")
-        override val datePropertyName: String get() = "this"
+        override val smallestUnit: TemporalUnitDescription get() = TemporalUnitDescription.DAYS
         override val simpleName: String get() = "date"
+        override val isDateBased: Boolean = true
     },
     DateTime {
         override val typeName: TypeName get() = base("DateTime")
+        override val smallestUnit: TemporalUnitDescription get() = TemporalUnitDescription.NANOSECONDS
         override val datePropertyName: String get() = "date"
         override val simpleName: String get() = "date-time"
+        override val isDateBased: Boolean = true
     },
     OffsetDateTime {
         override val typeName: TypeName get() = base("OffsetDateTime")
+        override val smallestUnit: TemporalUnitDescription get() = TemporalUnitDescription.NANOSECONDS
         override val datePropertyName: String get() = "dateTime"
         override val simpleName: String get() = "date-time"
+        override val isDateBased: Boolean = true
     },
     ZonedDateTime {
         override val typeName: TypeName get() = base("ZonedDateTime")
+        override val smallestUnit: TemporalUnitDescription get() = TemporalUnitDescription.NANOSECONDS
         override val datePropertyName: String get() = "dateTime"
         override val simpleName: String get() = "date-time"
+        override val isDateBased: Boolean = true
     },
     Instant {
         override val typeName: TypeName get() = base("Instant")
-        override val datePropertyName: String get() = ""
+        override val smallestUnit: TemporalUnitDescription get() = TemporalUnitDescription.NANOSECONDS
         override val simpleName: String get() = "instant"
     };
 
     abstract val typeName: TypeName
-    abstract val datePropertyName: String
+    abstract val smallestUnit: TemporalUnitDescription
     abstract val simpleName: String
 
-    val isDateBased: Boolean get() = datePropertyName.isNotEmpty()
+    open val isDateBased: Boolean = false
+
+    open val datePropertyName: String get() = throw NotImplementedError()
+
+    open fun convertsDirectlyTo(other: DateTimeDescription): Boolean {
+        return isDateBased &&
+            other.isDateBased &&
+            other.smallestUnit > smallestUnit &&
+            other.smallestUnit > TemporalUnitDescription.DAYS
+    }
 }
--- tools/code-generator/src/main/kotlin/io/islandtime/codegen/generators/DatePropertiesGenerator.kt
+++ tools/code-generator/src/main/kotlin/io/islandtime/codegen/generators/DatePropertiesGenerator.kt
@@ -6,6 +6,8 @@ import io.islandtime.codegen.SingleFileGenerator
 import io.islandtime.codegen.calendar
 import io.islandtime.codegen.descriptions.DateTimeDescription
 import io.islandtime.codegen.descriptions.DateTimeDescription.Date
+import io.islandtime.codegen.descriptions.TemporalUnitDescription.DAYS
+import io.islandtime.codegen.dsl.FileBuilder
 import io.islandtime.codegen.dsl.file
 import io.islandtime.codegen.internal
 import io.islandtime.codegen.measures
@@ -19,201 +21,204 @@ private fun buildDatePropertiesFile() = file(
     fileName = "_DateProperties",
     jvmName = "DateTimesKt"
 ) {
-    DateTimeDescription.values().filter { it.isDateBased }.forEach { receiverClass ->
-        property(name = "weekOfMonth", returnType = Int::class) {
-            kdoc { "The week of the month (0-5) according to the ISO definition." }
-            receiver(receiverClass.typeName)
+    DateTimeDescription.values()
+        .filter { it.isDateBased && it.smallestUnit <= DAYS }
+        .forEach { buildDatePropertiesForClass(it) }
+}
 
-            if (receiverClass == Date) {
-                getter {
-                    using("impl", internal("weekOfMonthImpl"))
-                    "return %impl:T"
-                }
-            } else {
-                modifiers(KModifier.INLINE)
-                delegatesTo(receiverClass.datePropertyName)
+private fun FileBuilder.buildDatePropertiesForClass(receiverClass: DateTimeDescription) {
+    property(name = "weekOfMonth", returnType = Int::class) {
+        kdoc { "The week of the month (0-5) according to the ISO definition." }
+        receiver(receiverClass.typeName)
+
+        if (receiverClass == Date) {
+            getter {
+                using("impl", internal("weekOfMonthImpl"))
+                "return %impl:T"
             }
+        } else {
+            modifiers(KModifier.INLINE)
+            delegatesTo(receiverClass.datePropertyName)
+        }
+    }
+
+    function(name = "weekOfMonth") {
+        kdoc { "The week of the month (0-5) according to the week definition in [settings]." }
+        receiver(receiverClass.typeName)
+        argument("settings", calendar("WeekSettings"))
+        returns(Int::class)
+
+        if (receiverClass == Date) {
+            code {
+                using("impl", internal("weekOfMonthImpl"))
+                "return %impl:T(%settings:N)"
+            }
+        } else {
+            delegatesTo(receiverClass.datePropertyName)
+        }
+    }
+
+    property(name = "weekOfYear", returnType = Int::class) {
+        kdoc {
+            """
+                The week of the year according to the ISO week definition. If the week number is associated with the
+                preceding year, `0` will be returned.
+
+                To obtain the week number used in the ISO week date system, use [weekOfWeekBasedYear] instead.
+
+                @see weekOfWeekBasedYear
+            """.trimIndent()
         }
 
-        function(name = "weekOfMonth") {
-            kdoc { "The week of the month (0-5) according to the week definition in [settings]." }
-            receiver(receiverClass.typeName)
-            argument("settings", calendar("WeekSettings"))
-            returns(Int::class)
+        receiver(receiverClass.typeName)
 
-            if (receiverClass == Date) {
-                code {
-                    using("impl", internal("weekOfMonthImpl"))
-                    "return %impl:T(%settings:N)"
-                }
-            } else {
-                delegatesTo(receiverClass.datePropertyName)
+        if (receiverClass == Date) {
+            getter {
+                using("impl", internal("weekOfYearImpl"))
+                "return %impl:T"
             }
+        } else {
+            modifiers(KModifier.INLINE)
+            delegatesTo(receiverClass.datePropertyName)
+        }
+    }
+
+    function("weekOfYear") {
+        kdoc {
+            """
+                The week of the year according to the week definition in [settings]. If the week number is associated
+                with the preceding year, `0` will be returned.
+
+                To obtain the week number of the week-based year, use [weekOfWeekBasedYear] instead.
+                
+                @see weekOfWeekBasedYear
+            """.trimIndent()
         }
 
-        property(name = "weekOfYear", returnType = Int::class) {
-            kdoc {
-                """
-                    The week of the year according to the ISO week definition. If the week number is associated with the
-                    preceding year, `0` will be returned.
+        receiver(receiverClass.typeName)
+        argument("settings", calendar("WeekSettings"))
+        returns(Int::class)
 
-                    To obtain the week number used in the ISO week date system, use [weekOfWeekBasedYear] instead.
- 
-                    @see weekOfWeekBasedYear
-                """.trimIndent()
+        if (receiverClass == Date) {
+            code {
+                using("impl", internal("weekOfYearImpl"))
+                "return %impl:T(%settings:N)"
             }
+        } else {
+            delegatesTo(receiverClass.datePropertyName)
+        }
+    }
 
-            receiver(receiverClass.typeName)
-
-            if (receiverClass == Date) {
-                getter {
-                    using("impl", internal("weekOfYearImpl"))
-                    "return %impl:T"
-                }
-            } else {
-                modifiers(KModifier.INLINE)
-                delegatesTo(receiverClass.datePropertyName)
-            }
+    property(name = "weekBasedYear", returnType = Int::class) {
+        kdoc {
+            """
+                The week-based year used in the ISO [week·date·system](https://en.wikipedia.org/wiki/ISO_week_date).
+                This differs from the regular ISO year when the week number falls in the preceding or following year.
+                
+                @see weekOfWeekBasedYear
+            """.trimIndent()
         }
 
-        function("weekOfYear") {
-            kdoc {
-                """
-                    The week of the year according to the week definition in [settings]. If the week number is
-                    associated with the preceding year, `0` will be returned.
+        receiver(receiverClass.typeName)
 
-                    To obtain the week number of the week-based year, use [weekOfWeekBasedYear] instead.
-                    
-                    @see weekOfWeekBasedYear
-                """.trimIndent()
+        if (receiverClass == Date) {
+            getter {
+                using("impl", internal("weekBasedYearImpl"))
+                "return %impl:T"
             }
+        } else {
+            modifiers(KModifier.INLINE)
+            delegatesTo(receiverClass.datePropertyName)
+        }
+    }
 
-            receiver(receiverClass.typeName)
-            argument("settings", calendar("WeekSettings"))
-            returns(Int::class)
-
-            if (receiverClass == Date) {
-                code {
-                    using("impl", internal("weekOfYearImpl"))
-                    "return %impl:T(%settings:N)"
-                }
-            } else {
-                delegatesTo(receiverClass.datePropertyName)
-            }
+    function(name = "weekBasedYear") {
+        kdoc {
+            """
+                The week-based year according to the week definition in [settings]. This differs from the regular ISO
+                year when the week number falls in the preceding or following year.
+                
+                @see weekOfWeekBasedYear
+            """.trimIndent()
         }
 
-        property(name = "weekBasedYear", returnType = Int::class) {
-            kdoc {
-                """
-                    The week-based year used in the ISO [week·date·system](https://en.wikipedia.org/wiki/ISO_week_date).
-                    This differs from the regular ISO year when the week number falls in the preceding or following
-                    year.
-                    
-                    @see weekOfWeekBasedYear
-                """.trimIndent()
-            }
+        receiver(receiverClass.typeName)
+        argument("settings", calendar("WeekSettings"))
+        returns(Int::class)
 
-            receiver(receiverClass.typeName)
-
-            if (receiverClass == Date) {
-                getter {
-                    using("impl", internal("weekBasedYearImpl"))
-                    "return %impl:T"
-                }
-            } else {
-                modifiers(KModifier.INLINE)
-                delegatesTo(receiverClass.datePropertyName)
+        if (receiverClass == Date) {
+            code {
+                using("impl", internal("weekBasedYearImpl"))
+                "return %impl:T(%settings:N)"
             }
+        } else {
+            delegatesTo(receiverClass.datePropertyName)
+        }
+    }
+
+    property(name = "weekOfWeekBasedYear", returnType = Int::class) {
+        kdoc {
+            """
+                The week number used in the ISO [week·date·system](https://en.wikipedia.org/wiki/ISO_week_date).
+                
+                @see weekBasedYear
+            """.trimIndent()
         }
 
-        function(name = "weekBasedYear") {
-            kdoc {
-                """
-                    The week-based year according to the week definition in [settings]. This differs from the regular
-                    ISO year when the week number falls in the preceding or following year.
-                    
-                    @see weekOfWeekBasedYear
-                """.trimIndent()
-            }
+        receiver(receiverClass.typeName)
 
-            receiver(receiverClass.typeName)
-            argument("settings", calendar("WeekSettings"))
-            returns(Int::class)
-
-            if (receiverClass == Date) {
-                code {
-                    using("impl", internal("weekBasedYearImpl"))
-                    "return %impl:T(%settings:N)"
-                }
-            } else {
-                delegatesTo(receiverClass.datePropertyName)
+        if (receiverClass == Date) {
+            getter {
+                using("impl", internal("weekOfWeekBasedYearImpl"))
+                "return %impl:T"
             }
+        } else {
+            modifiers(KModifier.INLINE)
+            delegatesTo(receiverClass.datePropertyName)
+        }
+    }
+
+    function(name = "weekOfWeekBasedYear") {
+        kdoc {
+            """
+                The week number of the week-based year according to the week definition in [settings].
+                
+                @see weekBasedYear
+            """.trimIndent()
         }
 
-        property(name = "weekOfWeekBasedYear", returnType = Int::class) {
-            kdoc {
-                """
-                    The week number used in the ISO [week·date·system](https://en.wikipedia.org/wiki/ISO_week_date).
-                    
-                    @see weekBasedYear
-                """.trimIndent()
-            }
+        receiver(receiverClass.typeName)
+        argument("settings", calendar("WeekSettings"))
+        returns(Int::class)
 
-            receiver(receiverClass.typeName)
-
-            if (receiverClass == Date) {
-                getter {
-                    using("impl", internal("weekOfWeekBasedYearImpl"))
-                    "return %impl:T"
-                }
-            } else {
-                modifiers(KModifier.INLINE)
-                delegatesTo(receiverClass.datePropertyName)
+        if (receiverClass == Date) {
+            code {
+                using("impl", internal("weekOfWeekBasedYearImpl"))
+                "return %impl:T(%settings:N)"
             }
+        } else {
+            delegatesTo(receiverClass.datePropertyName)
+        }
+    }
+
+    property(name = "lengthOfWeekBasedYear", returnType = measures("IntWeeks")) {
+        kdoc {
+            """
+                The length of the ISO week-based year that this ${receiverClass.simpleName} falls in, either 52 or 53
+                weeks.
+            """.trimIndent()
         }
 
-        function(name = "weekOfWeekBasedYear") {
-            kdoc {
-                """
-                    The week number of the week-based year according to the week definition in [settings].
-                    
-                    @see weekBasedYear
-                """.trimIndent()
+        receiver(receiverClass.typeName)
+
+        if (receiverClass == Date) {
+            getter {
+                using("impl", internal("lengthOfWeekBasedYear"))
+                "return %impl:T(weekBasedYear)"
             }
-
-            receiver(receiverClass.typeName)
-            argument("settings", calendar("WeekSettings"))
-            returns(Int::class)
-
-            if (receiverClass == Date) {
-                code {
-                    using("impl", internal("weekOfWeekBasedYearImpl"))
-                    "return %impl:T(%settings:N)"
-                }
-            } else {
-                delegatesTo(receiverClass.datePropertyName)
-            }
-        }
-
-        property(name = "lengthOfWeekBasedYear", returnType = measures("IntWeeks")) {
-            kdoc {
-                """
-                    The length of the ISO week-based year that this ${receiverClass.simpleName} falls in, either 52 or
-                    53 weeks.
-                """.trimIndent()
-            }
-
-            receiver(receiverClass.typeName)
-
-            if (receiverClass == Date) {
-                getter {
-                    using("impl", internal("lengthOfWeekBasedYear"))
-                    "return %impl:T(weekBasedYear)"
-                }
-            } else {
-                modifiers(KModifier.INLINE)
-                delegatesTo(receiverClass.datePropertyName)
-            }
+        } else {
+            modifiers(KModifier.INLINE)
+            delegatesTo(receiverClass.datePropertyName)
         }
     }
 }
