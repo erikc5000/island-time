ddb9981e91b22b0e676b8ea7b5525a8f9fd1ebda
Erik Christensen <40830816+erikc5000@users.noreply.github.com>
2020-08-16 13:58:32 -0400

    Fix visibility of nowImpl() (#124)

--- core/src/commonMain/kotlin/io/islandtime/clock/internal/NowImpl.kt
+++ core/src/commonMain/kotlin/io/islandtime/clock/internal/NowImpl.kt
@@ -3,9 +3,9 @@ package io.islandtime.clock.internal
 import io.islandtime.*
 import io.islandtime.clock.Clock
 
-expect fun Date.Companion.nowImpl(clock: Clock): Date
-expect fun DateTime.Companion.nowImpl(clock: Clock): DateTime
-expect fun OffsetDateTime.Companion.nowImpl(clock: Clock): OffsetDateTime
-expect fun ZonedDateTime.Companion.nowImpl(clock: Clock): ZonedDateTime
-expect fun Time.Companion.nowImpl(clock: Clock): Time
-expect fun OffsetTime.Companion.nowImpl(clock: Clock): OffsetTime
+internal expect fun Date.Companion.nowImpl(clock: Clock): Date
+internal expect fun DateTime.Companion.nowImpl(clock: Clock): DateTime
+internal expect fun OffsetDateTime.Companion.nowImpl(clock: Clock): OffsetDateTime
+internal expect fun ZonedDateTime.Companion.nowImpl(clock: Clock): ZonedDateTime
+internal expect fun Time.Companion.nowImpl(clock: Clock): Time
+internal expect fun OffsetTime.Companion.nowImpl(clock: Clock): OffsetTime
