f6b84d854ae8ab2cf238c5f64b663ec1ff98c164
Erik Christensen <erikchristensen@comcast.net>
2020-08-17 11:51:48 -0400

    Bump Kotlin to 1.4.0 final

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,18 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.0"
+    `kotlin-dsl`
+}
+
 repositories {
     jcenter()
     google()
 }
 
-plugins {
-    `kotlin-dsl`
-}
-
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0-rc")
+    implementation(kotlin("gradle-plugin"))
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:0.10.1")
     implementation("com.android.tools.build:gradle:4.0.1")
 }
 
 kotlinDslPluginOptions {
     experimentalWarning.set(false)
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
