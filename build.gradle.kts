5e3afde6d9b3987d84eb82a1dbf6fe8c3de8db10
Erik Christensen <40830816+erikc5000@users.noreply.github.com>
2020-11-23 22:45:02 -0500

    Gradle script tweaks (#154)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.dokka.gradle.DokkaTask
 
 buildscript {
@@ -14,26 +11,6 @@ buildscript {
 }
 
 subprojects {
-    tasks.withType<JavaCompile>().configureEach {
-        sourceCompatibility = JavaVersion.VERSION_1_8.toString()
-        targetCompatibility = JavaVersion.VERSION_1_8.toString()
-    }
-
-    tasks.withType<KotlinCompile>().configureEach {
-        kotlinOptions {
-            freeCompilerArgs = listOf("-XXLanguage:+InlineClasses")
-            jvmTarget = "1.8"
-        }
-    }
-
-    tasks.withType(AbstractTestTask::class).configureEach {
-        testLogging {
-            events = setOf(TestLogEvent.FAILED)
-            exceptionFormat = TestExceptionFormat.FULL
-            showStackTraces = true
-        }
-    }
-
     tasks.withType<DokkaTask>().configureEach {
         dokkaSourceSets {
             configureEach {
