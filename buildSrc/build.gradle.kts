4230ac48c30d9b1fc21af20476263755e275deb2
Erik Christensen <erikchristensen@comcast.net>
2021-05-24 17:50:30 -0400

    Bump Kotlin to 1.5.10

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin", "1.5.0"))
+    implementation(kotlin("gradle-plugin", "1.5.10"))
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
     implementation("com.android.tools.build:gradle:4.1.3")
     implementation("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.16.1")
