f1c798477d4b150fa3da319026a818e9698add5c
Erik Christensen <erikchristensen@comcast.net>
2021-07-11 21:17:36 -0400

    Bump AtomicFU to 0.16.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,5 +6,5 @@ dependencies {
     implementation(kotlin("gradle-plugin", "1.5.20"))
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
     implementation("com.android.tools.build:gradle:4.1.3")
-    implementation("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.16.1")
+    implementation("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.16.2")
 }
