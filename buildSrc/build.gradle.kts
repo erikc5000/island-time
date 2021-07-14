c976801a17ffac2fc0fc7bbe8122e6fcd4a6e14c
Erik Christensen <40830816+erikc5000@users.noreply.github.com>
2021-07-14 11:10:32 -0400

    Bump Kotlin to 1.5.21 (#202)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin", "1.5.20"))
+    implementation(kotlin("gradle-plugin", "1.5.21"))
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.5.0")
     implementation("com.android.tools.build:gradle:4.2.2")
     implementation("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.16.2")
