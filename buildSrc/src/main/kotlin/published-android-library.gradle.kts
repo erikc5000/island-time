26b612deb8aeff09c05917d21203884d2caeccf7
Erik Christensen <erikchristensen@comcast.net>
2022-02-16 09:14:44 -0500

    Simplify Android sources publishing

--- buildSrc/src/main/kotlin/published-android-library.gradle.kts
+++ buildSrc/src/main/kotlin/published-android-library.gradle.kts
@@ -5,20 +5,16 @@ plugins {
 
 android {
     publishing {
-        singleVariant("release")
+        singleVariant("release") {
+            withSourcesJar()
+        }
     }
 }
 
 afterEvaluate {
-    val androidSourcesJar by tasks.registering(Jar::class) {
-        archiveClassifier.set("sources")
-        from(android.sourceSets["main"].java.srcDirs)
-    }
-
     publishing {
         publications {
             create<MavenPublication>("releaseAar") {
-                artifact(androidSourcesJar.get())
                 from(components["release"])
             }
         }
