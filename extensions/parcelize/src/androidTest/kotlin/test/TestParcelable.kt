f058da04dd446c411e3849f5dce274452290fbbc
Erik Christensen <erikchristensen@comcast.net>
2023-05-05 09:37:34 -0400

    Fix deprecation warning

--- extensions/parcelize/src/androidTest/kotlin/test/TestParcelable.kt
+++ extensions/parcelize/src/androidTest/kotlin/test/TestParcelable.kt
@@ -1,5 +1,6 @@
 package io.islandtime.parcelize.test
 
+import android.os.Build
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
@@ -8,14 +9,24 @@ import com.google.common.truth.Truth.assertThat
 inline fun <reified T : Parcelable> testParcelable(parcelable: T) {
     val inBundle = Bundle().apply { putParcelable("data", parcelable) }
 
-    val outBundle = Parcel.obtain().run {
+    val outBundle = withParcel {
         writeBundle(inBundle)
         setDataPosition(0)
-        readBundle()
-    }!!.apply {
-        classLoader = T::class.java.classLoader
+        readBundle(T::class.java.classLoader)!!
     }
 
-    val result = outBundle.getParcelable<T>("data")
+    val result = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        outBundle.getParcelable("data", T::class.java)
+    } else {
+        @Suppress("DEPRECATION")
+        outBundle.getParcelable("data")
+    }
+
     assertThat(result).isEqualTo(parcelable)
+}
+
+inline fun <T> withParcel(block: Parcel.() -> T): T = Parcel.obtain().run {
+    val returnValue = block()
+    recycle()
+    returnValue
 }
