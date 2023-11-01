acfb304899e6fbb0bac084ce60803352bb918fb8
Erik Christensen <40830816+erikc5000@users.noreply.github.com>
2023-11-01 09:30:18 -0400

    Try again at fixing expect-actual warning

--- buildSrc/src/main/kotlin/base-convention.gradle.kts
+++ buildSrc/src/main/kotlin/base-convention.gradle.kts
@@ -4,22 +4,28 @@ import org.gradle.api.tasks.testing.AbstractTestTask
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 tasks.withType<JavaCompile>().configureEach {
     sourceCompatibility = JavaVersion.VERSION_11.toString()
     targetCompatibility = JavaVersion.VERSION_11.toString()
 }
 
-tasks.withType<KotlinCompile>().configureEach {
+tasks.withType<KotlinJvmCompile>().configureEach {
     compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_11)
+        jvmTarget = JvmTarget.JVM_11
+    }
+}
+
+tasks.withType<KotlinCompilationTask<*>>().configureEach {
+    compilerOptions {
         freeCompilerArgs.add("-Xexpect-actual-classes")
     }
 }
 
-tasks.withType(AbstractTestTask::class).configureEach {
+tasks.withType<AbstractTestTask>().configureEach {
     testLogging {
         events = setOf(TestLogEvent.FAILED)
         exceptionFormat = TestExceptionFormat.FULL
